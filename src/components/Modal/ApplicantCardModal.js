9c4f8b5c59c9d31dbd5a73814b02cc0ada8aedfb
PatrikJMelander <patrikjmelander@gmail.com>
2021-11-20 12:31:40 +0100

    fixed styling on modal

--- src/components/Modal/ApplicantCardModal.js
+++ src/components/Modal/ApplicantCardModal.js
@@ -1,18 +1,65 @@
 import React,{useState} from "react";
 import Modal from 'react-modal';
+import styled from 'styled-components';
+import {CloseO} from '@styled-icons/evil/CloseO';
 
 const customStyles = {
   content: {
+    position: 'absolute',
     top: '50%',
     left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
   },
 };
 
-// Make sure to bind modal to your appElement
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  background-color:#a5d6a7;
+  border-radius: 10px;
+
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledH4 = styled.h4`
+    margin-block-start: 1.33em;
+    margin-block-end: 0.3em;
+    margin-inline-start: 10px;
+    margin-inline-end: 0px;
+`;
+
+const StyledH5 = styled.h5`
+    margin-block-start: 0.5em;
+    margin-block-end: 0.1em;
+    margin-inline-start: 20px;
+    margin-inline-end: 0px;
+`;
+
+
+const StyledP = styled.p`
+    margin-left: 25px;
+    margin-right: 25px;
+    margin-top: 10px;
+`;
+
+const StyledCloseBtn = styled(CloseO)`
+    
+    top: 10px;     
+    cursor: pointer;
+    height: 28px;
+    width: 28px;
+    opacity: 1;
+    position: absolute;    
+    z-index: 999;
+    right: 10px;
+    color: #222;
+    text-decoration: none;
+    text-align: center;
+    line-height: 28px;
+`; 
+
 Modal.setAppElement('#root');
 
 
@@ -40,18 +87,22 @@ function ApplicantCardModal(props) {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <div>
-            <h4>Presentation</h4>
-            <p>{props.applicant.presentation}</p>
-            <h4>Arbetslivserfarenhet</h4>
+        <Container>
+            <StyledH4>Presentation</StyledH4>
+            <StyledP>{props.applicant.presentation}</StyledP>
+            <StyledH4>Arbetslivserfarenhet</StyledH4>
             {props.applicant.experience.map(experience =>(
-                 <p>{experience}</p>
+                <div>
+                    <StyledH5>{experience.title}</StyledH5>
+                    <StyledH5>{experience.period}</StyledH5>
+                    <StyledP>{experience.description}</StyledP>
+                </div>
             ))}
 
 
-        </div>
+        </Container>
 
-        <button onClick={closeModal}>close</button>
+        <StyledCloseBtn onClick={closeModal}></StyledCloseBtn>
       </Modal>
     </div>
   );
