9489221c13ead3ad8888b0ba177e683d9a340ad5
PatrikJMelander <patrikjmelander@gmail.com>
2021-12-20 14:16:50 +0100

    prep for adding competend on joboffer

---
+++ src/components/JobOfferPreview.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components'
+import {CloseO} from '@styled-icons/evil/CloseO';
+import colorPicker from '../testData/colorPicker';
+import Swal from 'sweetalert2';
+import Slider from '@mui/material/Slider';
+
+
+const Container = styled.div`
+    color: ${colorPicker.text};
+    font-family: 'Roboto', sans-serif; 
+    text-align: center;
+    background-color: ${colorPicker.primary};
+    width: 80%;
+    display: flex;
+    border-style: double;
+    border-color:  ${colorPicker.text};
+    margin-bottom: 10%;
+    margin-left: 10%;
+    margin-top: 10%;
+`;
+const LeftDiv = styled.div`
+    width: 25%;
+    border-right: solid;
+    border-color:  ${colorPicker.secondary};
+    padding: 5px;
+`;
+const RightDiv = styled.div`
+    padding: 5px;
+    width: 75%;
+`;
+
+const Preview = styled.div`
+    border-bottom: solid;
+    border-color:${colorPicker.secondary};
+`;
+const Title = styled.h1`
+    margin-right: 15px;
+    margin-top: 10px;
+`;
+
+const H5 = styled.h5`
+    margin-right: 15px;
+    margin-top: 10px;
+`;
+
+const Role = styled.p`
+    
+`;
+const Competence = styled.p`
+    
+`;
+
+
+const Competencies = styled.div`
+
+`;
+
+const StyledImg = styled.img`
+    border-radius: 100%;
+    text-align: center;
+    margin: 15px;
+`;
+
+
+function JobOfferPreview ({jobOffer}) {
+   
+    return (
+
+        <Container>
+            <LeftDiv>
+                <StyledImg src={jobOffer.imageUrl}></StyledImg>
+                <Preview>{jobOffer.preview} </Preview>
+
+                <Competencies> 
+                    <H5>Requested Competencies</H5>
+                    {jobOffer.competencies.map(competenceInmap =>{
+                        if(jobOffer.competencies[0]===""){
+                            return <p>no competencies specified</p>
+                        }else{
+                            return (<Competence>{competenceInmap.name} - {competenceInmap.years} years</Competence>)
+                        }
+                
+                
+            })}
+            
+            </Competencies>
+
+            </LeftDiv>
+            <RightDiv>
+                <Title>{jobOffer.title}</Title>
+                <H5>About the Company</H5>
+            <p>{jobOffer.companyDescription} </p>
+            <H5>Your Role</H5>
+            <Role>{jobOffer.aboutRole}</Role>
+            </RightDiv>
+        </Container>
+
+    )
+}
+
+export default JobOfferPreview;
--- src/testData/jobOfferingsTestData.js
+++ src/testData/jobOfferingsTestData.js
@@ -9,6 +9,11 @@ const jobOfferingsTestData = [
     companyDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus non ligula in ultricies. Quisque placerat ligula in blandit interdum. In sollicitudin quam in sem viverra fringilla. In malesuada metus at leo maximus, laoreet molestie dui sagittis. Ut eget facilisis urna. Ut dignissim venenatis scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam aliquet sit amet augue non iaculis. Proin dapibus at sem sed fringilla. Pellentesque vel rutrum turpis, sed vehicula magna. In vel est ac ex eleifend cursus. Phasellus ut eleifend arcu. Morbi id ante non erat pulvinar hendrerit a a massa. Quisque.",
     aboutRole: "nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut",
     imageUrl:"https://picsum.photos/250?random=1",
+    competencies:[
+      {id:"competence-1", name: "Java", years: 3},
+      {id:"competence-2", name: "Javascript", years: 1},
+      {id:"competence-3", name: "CSS", years: 2},
+    ],
     recruitmentSteps: [
       {
         id: 'recruitmentStep-1',
