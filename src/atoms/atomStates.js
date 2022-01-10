362dfeb1f3dbe53b631c805b2e3cad3653ead61e
PatrikJMelander <patrikjmelander@gmail.com>
2022-01-10 11:06:58 +0100

    created recoil atoms

---
+++ src/atoms/atomStates.js
@@ -0,0 +1,46 @@
+
+import { atom, selector } from "recoil";
+import candidateTestData from '../testData/candidateTestData';
+import jobOfferTestData from '../testData/jobOfferingsTestData';
+import colorPicker from '../testData/colorPicker';
+import animals from '../testData/animals';
+
+export const atomUser = atom({
+  key: "atomUser",
+  default: {},
+});
+
+export const atomAdmin = atom({
+  key: "atomAdmin",
+  default: false,
+});
+
+export const atomCandidates = atom({
+  key: "atomCandidates",
+  default: {candidateTestData},
+});
+
+export const atomJobOffers = atom({
+  key: "atomJobOffers",
+  default: {jobOfferTestData},
+});
+
+export const atomColorPicker = atom({
+  key: "atomColorPicker",
+  default: {colorPicker},
+});
+
+export const atomAnimals = atom({
+  key: "atomAnimals",
+  default: {animals},
+});
+
+export const selectorColorPicker = selector({
+  key: 'selectorColorPicker',
+  get: ({get}) => {
+    const colorPicker = get(atomColorPicker);
+
+        return colorPicker;
+    }
+  },
+);
